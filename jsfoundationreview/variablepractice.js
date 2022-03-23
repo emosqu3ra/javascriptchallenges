73bf768ac6155b9868c8a91508358b09cccf9dfc
E L Ortiz <elortiz@Es-MacBook-Air.local>
2022-03-23 19:33:29 -0400

    practice with variables and added new mkdir

---
+++ jsfoundationreview/variablepractice.js
@@ -0,0 +1,33 @@
+// EX OF PRIMITIVE DATA TYPES
+
+// LET VARIABLE CAN BE REASSIGNED IN VALUE 
+let hello = 'my name is steph';
+console.log(hello);
+
+// CONST VARIABLE CANNOT BE REASSIGNED
+const age = 'I am 30';
+console.log(hello, age);
+
+// EX OF NON PRIMITIVE DATA TYPES (ARRAY AND OBJECTS)
+let seasons =['Winter', 'Spring', 'Summer', 'Fall'];
+console.log(seasons[3]);
+
+const education = {
+    'literacy' : 'finances',
+    'expression' : 'student oriented'
+}
+
+console.log(education.literacy);
+
+// COMPARISONS USING OPERATORS 
+let help = 1200;
+help++;
+console.log(help);
+
+let g = 843939382;
+g = g + 1584554;
+console.log(g);
+
+let eat = 'taco bell'
+console.log('Yo quiero' + ' ' + 'taco bell' + '.');
+
